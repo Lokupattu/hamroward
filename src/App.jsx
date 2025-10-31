6c6ddb317751d91117bb0caa261030b8740ee602
lokupattu <lukegautam@gmail.com>
2025-10-31 19:51:41 +0545

    Initial commit - HamroWard

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const App = () => {
+  return (
+    <div className="mt-5 text-center justify-center font-extrabold text-9xl">
+      Hamro Ward (coming soon...)
+    </div>
+  );
+};
+
+export default App;
